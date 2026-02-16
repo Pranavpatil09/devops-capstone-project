1a5992378342640eedb7cae6eadb9c10f66a3525
Pranavpatil09 <85169238+Pranavpatil09@users.noreply.github.com>
2026-02-16 23:01:04 +0530

    variable.tf v2

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,3 +1,15 @@
 variable "region" {
- default = "ap-south-1"
+  default = "ap-south-1"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "ami" {
+  default = "ami-0f5ee92e2d63afc18"
+}
+
+variable "key_name" {
+  description = "Your existing AWS key pair name"
 }
