d9173b8623c2c9aafc12eca20364477b4b622aec
Pranavpatil09 <85169238+Pranavpatil09@users.noreply.github.com>
2026-02-18 04:28:38 +0530

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -6,8 +6,15 @@ variable "instance_type" {
   default = "t3.micro"
 }
 
-variable "ami" {
-  default = "ami-0f5ee92e2d63afc18"
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  owners = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
 }
 
 variable "key_name" {
