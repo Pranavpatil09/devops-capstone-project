1f9b6a05f52c05d7a8e12e84873c51532ad6581f
Pranavpatil09 <85169238+Pranavpatil09@users.noreply.github.com>
2026-04-28 02:08:15 +0530

    Update deployment message in app.py

--- app/app.py
+++ app/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def home():
-   return "My DevOps Capstone Project Successfully Deployed On EC2 Server!"
+   return "My DevOps Capstone Project Successfully Deployed!"
 @app.route("/health")
 def health():
    return {"status": "UP"}
